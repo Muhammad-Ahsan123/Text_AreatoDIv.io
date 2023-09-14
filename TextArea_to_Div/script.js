028a041bab1aa4fdaad2489e6bd32a4b9af5c17c
Muhammad-Ahsan123 <137956616+Muhammad-Ahsan123@users.noreply.github.com>
2023-09-14 16:56:29 +0500

    TextArea

--- TextArea_to_Div/script.js
+++
@@ -1,8 +0,0 @@
-let text = document.getElementById("inputTextArea");
-let button = document.getElementById("submitButton");
-let div_text = document.getElementById("outputDiv");
-
-button.addEventListener( 'click' , function(e){
-    textof_inputTextArea = inputTextArea.value;
-    outputDiv.textContent = textof_inputTextArea;
-})
